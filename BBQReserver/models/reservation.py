6863ab6fe30d04ad954ba20501df0f5a329fc14d
Konstantin <britikovki@yandex.ru>
2019-10-23 14:30:39 +0300

    improvement

--- BBQReserver/models/reservation.py
+++
@@ -1,8 +0,0 @@
-
-class  Reservation:
-   def __init__(self, userId, datetime, peopleNum, info , slot):
-       self.slot = slot
-       self.info = info
-       self.peopleNum = peopleNum
-       self.datetime = datetime
-       self.userId = userId
